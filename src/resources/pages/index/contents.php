495ae3494cfd6bc35253c44f795ace236dd195e2
Narrakas <zixing.narrakas1996@gmail.com>
2019-04-01 14:18:18 -0200

    Minor changes

--- src/resources/pages/index/contents.php
+++ src/resources/pages/index/contents.php
@@ -24,7 +24,7 @@
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="d-inline-block pt-3">
                                                 <div class="d-flex">
-                                                    <h2 class="mb-0"><?PHP \DynamicalWeb\HTML::print(CACHE_TOTAL_SERVERS); ?></h2>
+                                                    <h2 class="mb-0"><?PHP \DynamicalWeb\HTML::print(number_format(CACHE_TOTAL_SERVERS)); ?></h2>
                                                 </div>
                                                 <i class="mdi mdi-clock text-muted"></i>
                                                 <small class="text-gray">Updated 2 minutes ago.</small>
@@ -45,7 +45,7 @@
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="d-inline-block pt-3">
                                                 <div class="d-flex">
-                                                    <h2 class="mb-0">12,129</h2>
+                                                    <h2 class="mb-0"><?PHP \DynamicalWeb\HTML::print(number_format(CACHE_CURRENT_SESSIONS)); ?></h2>
                                                     <div class="d-flex align-items-center ml-2">
                                                         <i class="mdi mdi-clock text-muted"></i>
                                                         <small class=" ml-1 mb-0">Updated: 05:42pm</small>
