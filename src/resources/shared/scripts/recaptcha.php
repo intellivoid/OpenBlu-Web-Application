94cf01f7170ce04a61f4e63ed7c3b85a0f00dc0d
Narrakas <zixing.narrakas1996@gmail.com>
2019-04-01 12:28:39 -0200

    Added Recaptcha Module (DynaMod)

---
+++ src/resources/shared/scripts/recaptcha.php
@@ -0,0 +1,64 @@
+<?php
+
+    /**
+     * Returns the rendering content for recaptcha
+     *
+     * @return string
+     */
+    function re_render()
+    {
+        $configuration = \DynamicalWeb\DynamicalWeb::getConfiguration('recaptcha');
+
+        if($configuration['enabled'] == true)
+        {
+            return("<div class=\"g-recaptcha\" data-sitekey=\"" . $configuration['site_key'] . "\"></div>");
+        }
+
+        return '';
+    }
+
+    /**
+     * Verifies the post request of the captcha request
+     *
+     * @return bool
+     */
+    function verify_recaptcha(): bool
+    {
+        $configuration = \DynamicalWeb\DynamicalWeb::getConfiguration('recaptcha');
+
+        if($configuration['enabled'] == false)
+        {
+            return false;
+        }
+
+        // Check the captcha
+        if (!isset($_POST['g-recaptcha-response']))
+        {
+            return false;
+        }
+
+        $opts = array('http' =>
+            array(
+                'method' => 'POST ',
+                'header' => 'Content-type: application/x-www-form-urlencoded',
+                'content' => http_build_query(array(
+                    'secret' => (string)$configuration['secret_key'],
+                    'response' => $_POST['g-recaptcha-response'],
+                    'remoteip' => $_SERVER['REMOTE_ADDR']
+                ))
+            )
+        );
+
+        $context = stream_context_create($opts);
+        $response = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
+        $result = json_decode($response);
+
+        if(!$result->success)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
