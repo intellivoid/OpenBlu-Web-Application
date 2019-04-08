ca7f751bd9bb87ea3c436d82eb38d1d176f5af48
Narrakas <zixing.narrakas1996@gmail.com>
2019-04-08 09:36:35 -0400

    Added missing docs to alert.go (\resources\shared\scripts)

--- src/resources/shared/scripts/alert.php
+++ src/resources/shared/scripts/alert.php
@@ -1,7 +1,16 @@
 <?php
 
+    use DynamicalWeb\HTML;
+
+    /**
+     * Renders an Alert
+     *
+     * @param string $text
+     * @param string $type
+     * @param string $icon
+     */
     function render_alert(string $text, string $type, string $icon)
     {
-        \DynamicalWeb\HTML::print("<div class=\"alert alert-fill-$type animated flipInX\" role=\"alert\">", false);
-        \DynamicalWeb\HTML::print("<i class=\"mdi mdi-$icon\"></i>$text</div>", false);
+        HTML::print("<div class=\"alert alert-fill-$type animated flipInX\" role=\"alert\">", false);
+        HTML::print("<i class=\"mdi mdi-$icon\"></i>$text</div>", false);
     }
