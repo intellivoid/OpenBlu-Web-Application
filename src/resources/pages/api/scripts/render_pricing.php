e87882cde1e9dfc3df90b44c8e988da7fa469a13
narrakas <zixing.narrakas1996@gmail.com>
2019-04-11 15:04:27 -0400

    Added translation data for Basic pricing table

--- src/resources/pages/api/scripts/render_pricing.php
+++ src/resources/pages/api/scripts/render_pricing.php
@@ -11,7 +11,7 @@
                     <div class="col-md-4 grid-margin stretch-card pricing-card">
                         <div class="card border-primary border pricing-card-body">
                             <div class="text-center pricing-card-head">
-                                <h3><?PHP HTML::print(TEXT_PRICING_FREE_HEADER); ?></h3>
+                                <h3> >_ <?PHP HTML::print(TEXT_PRICING_FREE_HEADER); ?></h3>
                                 <p><?PHP HTML::print(TEXT_PRICING_FREE_SUB_HEADER); ?></p>
                                 <h1 class="font-weight-normal mb-4">$0.00</h1>
                             </div>
@@ -29,19 +29,19 @@
                     <div class="col-md-4 grid-margin stretch-card pricing-card">
                         <div class="card border border-success pricing-card-body">
                             <div class="text-center pricing-card-head">
-                                <h3 class="text-success">Basic</h3>
-                                <p>For Personal Use</p>
+                                <h3 class="text-success"> >_ <?PHP HTML::print(TEXT_PRICING_BASIC_HEADER); ?></h3>
+                                <p><?PHP HTML::print(TEXT_PRICING_BASIC_SUB_HEADER); ?></p>
                                 <h1 class="font-weight-normal mb-4">
                                     $7.99
-                                    <p class="text-muted">Monthly</p>
+                                    <p class="text-muted"><?PHP HTML::print(TEXT_PRICING_BASIC_CYCLE); ?></p>
                                 </h1>
 
                             </div>
                             <ul class="list-unstyled plan-features">
-                                <li>5000 Monthly Requests</li>
-                                <li>Public server listings</li>
-                                <li>OpenVPN configurations</li>
-                                <li>For personal use only</li>
+                                <li><?PHP HTML::print(TEXT_PRICING_BASIC_FEATURE_1) ?></li>
+                                <li><?PHP HTML::print(TEXT_PRICING_BASIC_FEATURE_2); ?></li>
+                                <li><?PHP HTML::print(TEXT_PRICING_BASIC_FEATURE_3); ?></li>
+                                <li><?PHP HTML::print(TEXT_PRICING_BASIC_FEATURE_4); ?></li>
                             </ul>
                             <div class="wrapper">
                                 <a href="#" class="btn btn-inverse-success btn-block">Purchase Now</a>
@@ -51,7 +51,7 @@
                     <div class="col-md-4 grid-margin stretch-card pricing-card">
                         <div class="card border border-primary pricing-card-body">
                             <div class="text-center pricing-card-head">
-                                <h3>Enterprise</h3>
+                                <h3> >_ Enterprise</h3>
                                 <p>For Large Scale Use</p>
                                 <h1 class="font-weight-normal mb-4">
                                     $39.99
