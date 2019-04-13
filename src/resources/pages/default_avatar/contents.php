072b69fd20e4b351b94d99f750c54da05d66c360
narrakas <zixing.narrakas1996@gmail.com>
2019-04-13 18:37:42 -0400

    Added /add_balance

--- src/resources/pages/default_avatar/contents.php
+++ src/resources/pages/default_avatar/contents.php
@@ -17,7 +17,7 @@
 
     function display_picture()
     {
-        $resource = file_get_contents('https://ui-avatars.com/api/?background=30353c&color=fff&name=' . urlencode(WEB_ACCOUNT_USERNAME));
+        $resource = file_get_contents('https://ui-avatars.com/api/?background=2e2f32&color=fff&name=' . urlencode(WEB_ACCOUNT_USERNAME));
 
         header('Content-Type: image/png');
         header('cache-control: max-age=172800');
