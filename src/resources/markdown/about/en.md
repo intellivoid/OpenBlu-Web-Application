e8a4c2c484b986129e78d6d36bb4ffcd6d5d11ba
Narrakas <netkas@users.noreply.github.com>
2019-05-25 18:46:22 -0400

    Added /about

---
+++ src/resources/markdown/about/en.md
@@ -0,0 +1,38 @@
+# What is this service about?
+
+This service was created by Intellivoid and hosted by Bytechain,
+the VPN Servers available in OpenBlu are not hosted by
+Intellivoid or Bytechain but rather a small community of
+volunteers.
+
+Any issues with VPN Servers are not the responsibility of
+Intellivoid.
+
+This service is provided entirely for free without any
+applied restrictions other than your own limitations, 
+you should be able to connect to **ANY** VPN Server for free
+using any device using a OpenVPN Client. Intellivoid will never
+charge for access to OpenBlu VPN Servers.
+
+The only paid services from OpenBlu is API Access which allows
+developers to write clients that retrieves server listings from
+OpenBlu and individual server configurations. The purpose
+of the API is just to retrieve information from OpenBlu's
+publicly available VPN Servers.
+
+This service, API and official clients were made with love by
+Intellivoid. This service would not be available to the public
+without the help of Bytechain's Technologies.
+
+If you want to keep this service alive and see future improvements
+you should consider donating to Intellivoid or purchasing an API
+Subscription.
+
+-------------------------------------------------------------------
+
+# Affiliation Program
+
+We are always opened to affiliates, but for that we need to
+have a discussion to understand the conditions and other
+possible questions, feel free to contact us [here](/support)
+and we will get back to you as soon as possible
---
+++ src/resources/pages/about/contents.php
@@ -0,0 +1,52 @@
+<?PHP
+    /** @noinspection PhpUnhandledExceptionInspection */
+
+    use DynamicalWeb\HTML;
+    use OpenBlu\Objects\VPN;
+    use OpenBlu\OpenBlu;
+
+    HTML::importScript('client_mode');
+    HTML::importScript('check_auth');
+    HTML::importScript('cache');
+
+    $OpenBlu = new OpenBlu();
+?>
+<!DOCTYPE html>
+<html lang="<?PHP HTML::print(APP_LANGUAGE_ISO_639); ?>">
+    <head>
+        <?PHP HTML::importSection('header'); ?>
+        <title><?PHP HTML::print(TEXT_PAGE_TITLE); ?></title>
+    </head>
+
+    <body>
+
+        <div class="container-scrollbar">
+            <?PHP HTML::importSection('navigation'); ?>
+            <div class="container-fluid page-body-wrapper">
+                <?PHP HTML::importSection('sidebar'); ?>
+                <div class="main-panel">
+                    <div class="content-wrapper">
+
+                        <div class="row">
+                            <div class="col-12 grid-margin">
+                                <div class="card animated fadeInUp">
+                                    <div class="card-body">
+                                        <h4 class="card-title"><?PHP HTML::print(TEXT_CARD_HEADER); ?></h4>
+                                        <?PHP HTML::importMarkdown('about'); ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+            <?PHP HTML::importSection('footer'); ?>
+        </div>
+
+        <?PHP HTML::importSection('js_scripts'); ?>
+
+    </body>
+</html>
