276a534881f2b9122695d9f8ee7957f405f198c6
Narrakas <netkas@users.noreply.github.com>
2019-05-25 19:29:20 -0400

    Removed unused libraries

--- src/resources/sections/js_scripts.php
+++ src/resources/sections/js_scripts.php
@@ -1,9 +1,3 @@
 <script src="/assets/vendors/js/vendor.bundle.base.js"></script>
 <script src="/assets/vendors/js/vendor.bundle.addons.js"></script>
-<script src="/assets/js/off-canvas.js"></script>
-<script src="/assets/js/hoverable-collapse.js"></script>
-<script src="/assets/js/misc.js"></script>
-<script src="/assets/js/settings.js"></script>
-<script src="/assets/js/todolist.js"></script>
-<script src="/assets/js/dashboard.js"></script>
 <script src="/assets/js/app.js"></script>
