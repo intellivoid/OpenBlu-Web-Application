2a601b3a1bd240aacbe3b8d030beb1fb16066e40
Narrakas <netkas@users.noreply.github.com>
2019-06-13 18:29:00 -0400

    Minor bug fix where partial.js.go throws an exception when requested but during when web resources are being loaded

--- src/resources/sections/js_scripts.php
+++ src/resources/sections/js_scripts.php
@@ -2,16 +2,20 @@
 <script src="/assets/vendors/js/vendor.bundle.addons.js"></script>
 <script src="/assets/js/app.js"></script>
 <?PHP
-    if(WEB_SESSION_ACTIVE == true)
+    if(defined('WEB_SESSION_ACTIVE') == true)
     {
-        ?>
-        <script src="/assets/js/jquery.letterpic.js"></script>
-        <script>
-            $("#user-avatar").letterpic({
-                fill: 'color',
-                colors: [ "#2e2f32" ]
-            });
-        </script>
-        <?PHP
+        if(WEB_SESSION_ACTIVE == true)
+        {
+            ?>
+            <script src="/assets/js/jquery.letterpic.js"></script>
+            <script>
+                $("#user-avatar").letterpic({
+                    fill: 'color',
+                    colors: [ "#2e2f32" ]
+                });
+            </script>
+            <?PHP
+        }
     }
+
 ?>
