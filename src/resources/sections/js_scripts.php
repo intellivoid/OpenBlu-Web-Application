d8723b492f12b4aeec5437112da150a5e6c1cfac
Narrakas <netkas@users.noreply.github.com>
2019-06-13 21:45:45 -0400

    Minor security patches to /vendors

--- src/resources/sections/js_scripts.php
+++ src/resources/sections/js_scripts.php
@@ -1,3 +1,4 @@
+<script src="/assets/js/jquery.js"></script>
 <script src="/assets/vendors/js/vendor.bundle.base.js"></script>
 <script src="/assets/vendors/js/vendor.bundle.addons.js"></script>
 <script src="/assets/js/app.js"></script>
