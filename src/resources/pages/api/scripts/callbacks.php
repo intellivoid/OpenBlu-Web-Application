bc429d21dcf51e5e79cac9d4fa600dd3a38aeb60
netkas <netkas@intellivoid.info>
2020-02-06 00:00:18 -0300

    Updated es translation

--- src/resources/pages/api/scripts/callbacks.php
+++ src/resources/pages/api/scripts/callbacks.php
@@ -9,15 +9,15 @@
                 break;
 
             case 101:
-                render_alert("The subscription plan was not found", 'danger', 'alert-circle');
+                render_alert(TEXT_CALLBACK_101, 'danger', 'alert-circle');
                 break;
 
             case 102:
-                render_alert("The subscription plan is not available", 'danger', 'alert-circle');
+                render_alert(TEXT_CALLBACK_102, 'danger', 'alert-circle');
                 break;
 
             case 103:
-                render_alert("A new access key has been generated successfully", 'success', 'check-circle-outline');
+                render_alert(TEXT_CALLBACK_103, 'success', 'check-circle-outline');
                 break;
 
         }
