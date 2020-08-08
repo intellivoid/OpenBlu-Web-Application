c0d2f634a79e20eb998767a0fb0515ca66d82993
netkas <netkas@intellivoid.info>
2020-08-08 14:35:19 -0400

    Updated DynamicalWeb

--- src/resources/DynamicalWeb/Runtime.php
+++ src/resources/DynamicalWeb/Runtime.php
@@ -4,6 +4,8 @@
     namespace DynamicalWeb;
 
     use Exception;
+    use ppm\ppm;
+    use RuntimeException;
 
     /**
      * Class Runtime
@@ -11,6 +13,13 @@
      */
     class Runtime
     {
+        /**
+         * Indicates if PPM has been loaded
+         *
+         * @var bool
+         */
+        public static $PpmLoaded = false;
+
         /**
          * Executes a runtime script
          *
@@ -58,43 +67,96 @@
 
             $configuration =  DynamicalWeb::getWebConfiguration();
 
+            if(self::$PpmLoaded == false)
+            {
+                if($configuration['enable_ppm'] == true)
+                {
+                    /** @noinspection PhpIncludeInspection */
+                    require("ppm");
+
+                    if(defined("PPM"))
+                    {
+                        self::$PpmLoaded = true;
+                    }
+                    else
+                    {
+                        throw new RuntimeException("Cannot start PPM, please make sure that PPM is installed properly.");
+                    }
+                }
+            }
+
             if(isset($configuration['libraries'][$library_name]) == false)
             {
                 throw new Exception('The library "' . $library_name . '" was not found in WebConfiguration');
             }
 
-            $LibrariesPath = APP_RESOURCES_DIRECTORY . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR;
-            $LibraryPath = $LibrariesPath . $configuration['libraries'][$library_name]['directory_name'];
-            $AutoloaderPath = $LibraryPath . DIRECTORY_SEPARATOR . $configuration['libraries'][$library_name]['autoloader'];
-
-            if(file_exists($LibrariesPath) == false)
+            if(isset($configuration['libraries'][$library_name]['package_name']))
             {
-                throw new Exception('The resources directory for libraries was not found');
-            }
-
-            if(file_exists($LibraryPath) == false)
-            {
-                throw new Exception('The directory for the library "' . $library_name . '" was not found');
-            }
-
-            if(file_exists($AutoloaderPath) == false)
-            {
-                throw new Exception('The autoloader for the library "' . $library_name . '" was not found');
-            }
-
-            if($configuration['libraries'][$library_name]['check_class_exists'] == true)
-            {
-                $Namespace = $configuration['libraries'][$library_name]['namespace'];
-                $ClassName = $configuration['libraries'][$library_name]['main_class'];
-
-                if(class_exists($Namespace . "\\" . $ClassName) == true)
+                // Import as a PPM package
+                if($configuration['enable_ppm'] == false)
                 {
-                    return;
+                    throw new Exception('The library "' . $library_name . '" cannot be imported because it\'s a PPM package and PPM is not enabled in the configuration');
                 }
+
+                $version = 'latest';
+                $import_dependencies = true;
+                $throw_error = true;
+
+                if(isset($configuration['libraries'][$library_name]['version']))
+                {
+                    $version = $configuration['libraries'][$library_name]['version'];
+                }
+
+                if(isset($configuration['libraries'][$library_name]['import_dependencies']))
+                {
+                    $import_dependencies = $configuration['libraries'][$library_name]['import_dependencies'];
+                }
+
+                if(isset($configuration['libraries'][$library_name]['throw_error']))
+                {
+                    $throw_error = $configuration['libraries'][$library_name]['throw_error'];
+                }
+
+                ppm::import($configuration['libraries'][$library_name]['package_name'], $version, $import_dependencies, $throw_error);
+            }
+            else
+            {
+                // Load from the libraries directory
+
+                $LibrariesPath = APP_RESOURCES_DIRECTORY . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR;
+                $LibraryPath = $LibrariesPath . $configuration['libraries'][$library_name]['directory_name'];
+                $AutoloaderPath = $LibraryPath . DIRECTORY_SEPARATOR . $configuration['libraries'][$library_name]['autoloader'];
+
+                if(file_exists($LibrariesPath) == false)
+                {
+                    throw new Exception('The resources directory for libraries was not found');
+                }
+
+                if(file_exists($LibraryPath) == false)
+                {
+                    throw new Exception('The directory for the library "' . $library_name . '" was not found');
+                }
+
+                if(file_exists($AutoloaderPath) == false)
+                {
+                    throw new Exception('The autoloader for the library "' . $library_name . '" was not found');
+                }
+
+                if($configuration['libraries'][$library_name]['check_class_exists'] == true)
+                {
+                    $Namespace = $configuration['libraries'][$library_name]['namespace'];
+                    $ClassName = $configuration['libraries'][$library_name]['main_class'];
+
+                    if(class_exists($Namespace . "\\" . $ClassName) == true)
+                    {
+                        return;
+                    }
+                }
+
+                /** @noinspection PhpIncludeInspection */
+                include_once($AutoloaderPath);
             }
 
-            /** @noinspection PhpIncludeInspection */
-            include_once($AutoloaderPath);
             DynamicalWeb::$loadedLibraries[] = $library_name;
             self::runEventScripts('on_import');
         }
